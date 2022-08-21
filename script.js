500962f58eba182e27d11d114f8db5e48c85cb51
Davi Belo <davibelocontato@gmail.com>
2022-08-21 02:50:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('You know it is not the same as it was')
+
+function change(){
+    document.getElementById('no1').style.display='none';
+    document.getElementById('no2').style.display='block'
+}
+
+function change1(){
+    document.getElementById('no2').style.display='none';
+    document.getElementById('no3').style.display='block'
+}
+
+function change2(){
+    document.getElementById('no3').style.display='none';
+    document.getElementById('no4').style.display='block'
+}
+
+
+function change3(){
+    document.getElementById('no4').style.display='none';
+    document.getElementById('no5').style.display='block'
+}
+
+
+function change4(){
+    document.getElementById('no5').style.display='none';
+    document.getElementById('no1').style.display='block'
+}
+
+
+
